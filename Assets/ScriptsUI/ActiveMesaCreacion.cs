a332d0a77b51e44689ef66cf41778cd36afd2bd7
urnice1111 <emilianogarramxd@gmail.com>
2026-03-13 04:13:15 -0600

    Fixed not saved changres for last commit

--- Assets/ScriptsUI/ActiveMesaCreacion.cs
+++ Assets/ScriptsUI/ActiveMesaCreacion.cs
@@ -3,14 +3,21 @@ using UnityEngine.UIElements;
 
 public class ActiveMesaCreacion : MonoBehaviour
 {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     [SerializeField] private UIDocument uiDocument;
+
     [SerializeField] private MesaCreacionScript mesaCreacion;
 
     private Button botonActivatePopUp;
 
+
+
     private void OnEnable()
     {
+        
         uiDocument = GetComponent<UIDocument>();
+
         var popUp = uiDocument.rootVisualElement;
 
         if (mesaCreacion == null)
@@ -18,15 +25,19 @@ public class ActiveMesaCreacion : MonoBehaviour
 
         botonActivatePopUp = popUp.Q<Button>("MesaCreacion");
         botonActivatePopUp.RegisterCallback<ClickEvent>(activatePopUp);
+
+
     }
+
 
     private void activatePopUp(ClickEvent evt)
     {
         if (mesaCreacion == null)
         {
-            Debug.LogError("MesaCreacionScript no encontrado en la escena.");
+            Debug.Log("nulo");
             return;
         }
         mesaCreacion.AbrirMesaCreacion();
     }
+
 }
