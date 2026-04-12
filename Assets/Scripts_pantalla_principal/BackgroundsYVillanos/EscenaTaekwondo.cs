eca771a2e882197b869010fb84323c04dac2c143
k4abrego <karenabrego182@gmail.com>
2026-04-12 17:26:14 -0600

    prueba 243

--- Assets/Scripts_pantalla_principal/BackgroundsYVillanos/EscenaTaekwondo.cs
+++ Assets/Scripts_pantalla_principal/BackgroundsYVillanos/EscenaTaekwondo.cs
@@ -2,21 +2,21 @@ using UnityEngine;
 
 public class EscenaTaekwondo : MonoBehaviour
 {
-    public float speed = 0.5f; //Velocidad a la que se mueve el fondo
-    private float width; //Ancho del fondo
+    public float velocidad  = 0.5f; //Velocidad a la que se mueve el fondo
+    private float ancho; //Ancho del fondo
 
     void Start()
     {
-        width = GetComponent<SpriteRenderer>().bounds.size.x; //Obtiene el ancho del fondo a partir del tamaño del sprite, para saber cuándo reiniciar la posición
+        ancho = GetComponent<SpriteRenderer>().bounds.size.x; //Obtiene el ancho del fondo a partir del tamaño del sprite, para saber cuándo reiniciar la posición
     }
 
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime); //Mueve el fondo hacia la izquierda, multiplicada por Time.deltaTime para que el movimiento sea suave
+        transform.Translate(Vector3.left * velocidad * Time.deltaTime); //Mueve el fondo hacia la izquierda, multiplicada por Time.deltaTime para que el movimiento sea suave
 
-        if (transform.position.x <= -width) //Si la posición del fondo es menor o igual a su ancho negativo, significa que ha salido completamente de la pantalla
+        if (transform.position.x <= -ancho) //Si la posición del fondo es menor o igual a su ancho negativo, significa que ha salido completamente de la pantalla
         {
-            transform.position += new Vector3(width * 2, 0, 0); //Para que vuelva a aparecer en la pantalla y crear un efecto de movimiento continuo (loop)
+            transform.position += new Vector3(ancho * 2, 0, 0); //Para que vuelva a aparecer en la pantalla y crear un efecto de movimiento continuo (loop)
         }
     }
 }
--- Assets/Scripts_pantalla_principal/BackgroundsYVillanos/MovimientoVillanoTae.cs
+++ Assets/Scripts_pantalla_principal/BackgroundsYVillanos/MovimientoVillanoTae.cs
@@ -5,6 +5,8 @@ public class MovimientoVillanoTae : MonoBehaviour
     public float velocidad = 2f; //Velocidad a la que se mueve el villano
     public float limite = 3f; //Distancia máxima que el villano se moverá desde su posición inicial antes de cambiar de dirección
     private Vector3 posicionInicial; //Almacena la posición inicial del villano para calcular los límites de movimiento
+    private float limiteIzquierdo; //Límite izquierdo de movimiento en X
+    private float limiteDerecho; //Límite derecho de movimiento en X
     private int direccion = 1; //Variable para controlar la dirección del movimiento, 1 para derecha y -1 para izquierda
     void Start()
     {
@@ -18,13 +20,13 @@ public class MovimientoVillanoTae : MonoBehaviour
         Vector3 nuevaPosicion = transform.position; //Crea una variable para almacenar la nueva posición del villano.
         nuevaPosicion.x += direccion * velocidad * Time.deltaTime; //Calcula la nueva posición del villano en el eje x.
 
-        if (transform.position.x >= limiteDerecho) //Si el villano alcanza o supera el límite derecho, cambia la dirección a la izquierda
+        if (nuevaPosicion.x >= limiteDerecho) //Si el villano alcanza o supera el límite derecho, cambia la dirección a la izquierda
         {
             nuevaPosicion.x = limiteDerecho;
             direccion = -1; //Cambia la dirección a la izquierda
         }
 
-        else if (transform.position.x <= limiteIzquierdo)
+        else if (nuevaPosicion.x <= limiteIzquierdo)
         {
             nuevaPosicion.x = limiteIzquierdo;
             direccion = 1; //Cambia la dirección a derecha
