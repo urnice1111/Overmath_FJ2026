634d4cb0151738169f8b815710eca7a01ba5650c
gabrielmc05 <gabo.manzo.c@gmail.com>
2026-04-12 10:57:14 -0600

    Joystick for the boat

---
+++ Assets/Scripts_pantalla_principal/Joystick.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    public RectTransform background;
+    public RectTransform handle;
+
+    private Vector2 input = Vector2.zero;
+
+    public Vector2 GetInput()
+    {
+        return input;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            background,
+            eventData.position,
+            eventData.pressEventCamera,
+            out pos
+        );
+        pos.x = pos.x / background.sizeDelta.x;
+        pos.y = pos.y / background.sizeDelta.y;
+
+        input = new Vector2(pos.x * 2, pos.y * 2);
+        input = input.magnitude > 1 ? input.normalized : input;
+
+        handle.anchoredPosition = new Vector2(
+            input.x * (background.sizeDelta.x / 2),
+            input.y * (background.sizeDelta.y / 2)
+        );
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        input = Vector2.zero;
+        handle.anchoredPosition = Vector2.zero;
+    }
+}
--- Assets/Scripts_pantalla_principal/MoverBarco.cs
+++ Assets/Scripts_pantalla_principal/MoverBarco.cs
@@ -1,38 +1,52 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-// Esta clase maneja el movimiento del barco en 2D 
+// Esta clase maneja el movimiento del barco en 2D con teclado y joystick táctil
 
 public class MoverBarco : MonoBehaviour
 {
     [SerializeField]
     private InputAction accionMover;
-
     private Rigidbody2D rb;
+
     [SerializeField]
     private float velocidadMovimiento = 7f;
-        
+
+    // Referencia al joystick 
+    [SerializeField]
+    private Joystick joystick;
+    private Vector2 input;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
     void OnEnable()
     {
         accionMover.Enable();
     }
-
     void OnDisable()
     {
         accionMover.Disable();
     }
+    void Update()
+    {
+        // Input de teclado
+        Vector2 inputTeclado = accionMover.ReadValue<Vector2>();
+        // Input táctil (joystick)
+        Vector2 inputTouch = Vector2.zero;
 
+        if (joystick != null)
+        {
+            inputTouch = joystick.GetInput();
+        }
+        // Combinar ambos inputs
+        input = inputTeclado + inputTouch;
+        // Evitar que sea mayor a 1 (normalizar)
+        input = Vector2.ClampMagnitude(input, 1f);
+    }
     void FixedUpdate()
     {
-        Vector2 movimiento = accionMover.ReadValue<Vector2>();
-        rb.linearVelocity = movimiento * velocidadMovimiento;
+        rb.linearVelocity = input * velocidadMovimiento;
     }
 }
 
-//git checkout --theirs Assets/Scenes/VistaSeleccion.unity.meta
-//git add Assets/Scenes/VistaSeleccion.unity.meta
