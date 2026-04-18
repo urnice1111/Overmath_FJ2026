492019c042cd55a94449228ff1065c4a9add01eb
k4abrego <karenabrego182@gmail.com>
2026-04-17 20:30:07 -0600

    base prueba

---
+++ Assets/Scripts/Pruebas/PruebasU.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+//using System.Collections.Generic;
+//using System.Reflection;
+using NUnit.Framework;
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class PrubasU
+{
+    private MoverBarco moverBarco;
+    private GameSession gameSession;
+}
