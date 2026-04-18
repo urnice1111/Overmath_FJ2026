0e38f25811bc8ffeec000fead7bb969ade416085
k4abrego <karenabrego182@gmail.com>
2026-04-17 20:36:38 -0600

    uuuuu

--- Assets/Scripts/Pruebas/PruebasU.cs
+++ Assets/Scripts/Pruebas/PruebasU.cs
@@ -10,4 +10,5 @@ public class PrubasU
 {
     private MoverBarco moverBarco;
     private GameSession gameSession;
+    
 }
--- Assets/Scripts/Scripts_pantalla_principal/EstadoBarco.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class EstadoBarco : MonoBehaviour
-{
-    // Placeholder para el barco 2D.
-}
