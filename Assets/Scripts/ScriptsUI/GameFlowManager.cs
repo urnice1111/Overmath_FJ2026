249121bdc23a3979591cf8052576debc984e0cd6
Regiina Hernandez <A01802688@tec.mx>
2026-04-19 16:36:54 -0600

    Canvas

--- Assets/Scripts/ScriptsUI/GameFlowManager.cs
+++ Assets/Scripts/ScriptsUI/GameFlowManager.cs
@@ -2,33 +2,33 @@ using UnityEngine;
 
 public class GameFlowManager : MonoBehaviour
 {
-    [SerializeField] private GameObject winPopup;   // Pop-up de victoria
-    [SerializeField] private GameObject losePopup;  // Pop-up de derrota
+    [SerializeField] private GameObject WinCanvas;   // Pop-up de victoria
+    [SerializeField] private GameObject LoseCanvas;  // Pop-up de derrota
 
-    // void Update()
-    // {
-    //     // Cuando se acaba el tiempo
-    //     if (TiempoJuego.Instance != null && TiempoJuego.Instance.TiempoRestante <= 0)
-    //     {
-    //         if (PuntajedePregunta.Instance.PuntosActuales >= 100)
-    //         {
-    //             // Victoria
-    //             winPopup.SetActive(true);
-    //         }
-    //         else
-    //         {
-    //             // Derrota
-    //             losePopup.SetActive(true);
-    //         }
+     void Update()
+     {
+         // Cuando se acaba el tiempo
+         if (TiempoJuego.Instance && TiempoJuego.Instance.TiempoRestante <= 0)
+         {
+             if (PuntajedePregunta.Instance.PuntosActuales >= 100)
+             {
+                 // Victoria
+                 WinCanvas.SetActive(true);
+             }
+             else
+             {
+                 // Derrota
+                 LoseCanvas.SetActive(true);
+             }
 
-    //         Time.timeScale = 0f; // Pausa el juego
-    //     }
+             Time.timeScale = 0f; // Pausa el juego
+         }
 
-    //     // Victoria inmediata si alcanza el puntaje antes de que se acabe el tiempo
-    //     if (PuntajedePregunta.Instance.PuntosActuales >= 100)
-    //     {
-    //         winPopup.SetActive(true);
-    //         Time.timeScale = 0f;
-    //     }
-    // }
+         // Victoria inmediata si alcanza el puntaje antes de que se acabe el tiempo
+         if (PuntajedePregunta.Instance.PuntosActuales >= 100)
+         {
+             WinCanvas.SetActive(true);
+             Time.timeScale = 0f;
+         }
+    }
 }
---
+++ Assets/Tests/PlayMode/PantallaPrincipalTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class PantallaPrincipalTests
+{
+    private GameObject gameSessionObject;
+    private GameSession gameSession;
+
+    [SetUp]
+    public void SetUp()
+    {
+        gameSessionObject = new GameObject("GameSession");
+        gameSession = gameSessionObject.AddComponent<GameSession>();
+    }
+
+    [Test]
+    public void GameSession_GuardaIslaYDificultad()
+    {
+        gameSession.SetIsla("isla_suma");
+        gameSession.SetDificultad(Dificultad.Facil);
+
+        Assert.AreEqual("isla_suma", GameSession.Instance.IslaActual);
+        Assert.AreEqual(Dificultad.Facil, GameSession.Instance.DificultadActual);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Object.DestroyImmediate(gameSessionObject);
+    }
+}
