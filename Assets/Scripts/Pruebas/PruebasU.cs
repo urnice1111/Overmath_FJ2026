6c25ef659f8089a37b876630aec74600ce1944a9
k4abrego <karenabrego182@gmail.com>
2026-04-18 21:15:53 -0600

    aun no queda la prueba

--- Assets/Scripts/Pruebas/PruebasU.cs
+++ Assets/Scripts/Pruebas/PruebasU.cs
@@ -3,12 +3,26 @@ using System.Collections;
 //using System.Reflection;
 using NUnit.Framework;
 using UnityEngine;
-//using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 public class PrubasU
 {
     private MoverBarco moverBarco;
     private GameSession gameSession;
-    
+
+    // [UnitySetUp]
+    // public IEnumerator SetUp()
+    // {
+    //     // Cargar la escena de prueba
+    //     SceneManager.LoadScene("OperandosEnterosScene");
+    //     yield return null;
+        
+
+    // }
+
 }
+
+//cada unitytest es un caso de prueba 
+//prueba unitaria 
+//prueba de integración 
