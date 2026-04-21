ca5ae8dd171dd943bdc4118af879f5d3e9e3f752
gabrielmc05 <gabo.manzo.c@gmail.com>
2026-04-21 15:06:23 -0600

    Changed the boat animation

--- Assets/Scripts/Scripts_pantalla_principal/CambiaAnimacion.cs
+++ Assets/Scripts/Scripts_pantalla_principal/CambiaAnimacion.cs
@@ -1,13 +1,14 @@
-using Unity.VisualScripting;
 using UnityEngine;
-
-// Esta clase cambia las animaciones del barco según su movimiento.
 
 public class CambiaAnimacion : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator animator;
     private SpriteRenderer sr;
+
+    private string ultimaDireccion = "down";
+
+    private bool mirandoIzquierda = false; 
 
     void Start()
     {
@@ -19,20 +20,34 @@ public class CambiaAnimacion : MonoBehaviour
     void Update()
     {
         Vector2 velocidad = rb.linearVelocity;
-        bool estaQuieto = velocidad.sqrMagnitude < 0.01f;
+
         bool mueveHorizontal = Mathf.Abs(velocidad.x) > 0.1f;
         bool mueveArriba = velocidad.y > 0.1f;
         bool mueveAbajo = velocidad.y < -0.1f;
-        
-        
-        animator.SetBool("idle", estaQuieto);
-        animator.SetBool("move_horizontal", mueveHorizontal);
-        animator.SetBool("move_up", mueveArriba);
-        animator.SetBool("move_down", mueveAbajo);
 
-        //animator.SetTrigger("")
+        if (mueveArriba)
+        {
+            ultimaDireccion = "up";
+        }
+        else if (mueveAbajo)
+        {
+            ultimaDireccion = "down";
+        }
+        else if (mueveHorizontal)
+        {
+            ultimaDireccion = "horizontal";
+        }
 
-        sr.flipX = velocidad.x < -0.1f;
+        animator.SetBool("move_up", ultimaDireccion == "up");
+        animator.SetBool("move_down", ultimaDireccion == "down");
+        animator.SetBool("move_horizontal", ultimaDireccion == "horizontal");
+
+        animator.SetBool("idle", false);
+        if (Mathf.Abs(velocidad.x) > 0.1f)
+        {
+            mirandoIzquierda = velocidad.x < 0;
+        }
+        sr.flipX = mirandoIzquierda;
     }
 }
 
