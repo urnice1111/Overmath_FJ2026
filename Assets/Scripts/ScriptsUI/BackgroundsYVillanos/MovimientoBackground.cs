d34e542b2fab7f03b05b5be122958aee0c093832
k4abrego <karenabrego182@gmail.com>
2026-04-23 08:27:12 -0600

    chance y ya quedó la rotación bien

--- Assets/Scripts/ScriptsUI/BackgroundsYVillanos/MovimientoBackground.cs
+++ Assets/Scripts/ScriptsUI/BackgroundsYVillanos/MovimientoBackground.cs
@@ -3,19 +3,25 @@ using UnityEngine;
 public class MovimientoBackground : MonoBehaviour
 {
     public float velocidad = 0.05f;
+    public float velocidadRotacion = 180f; //rotation speed in degrees per second
     private float ancho;
+    private Vector3 posicionInicial;
 
     void Start()
     {
         ancho = GetComponent<SpriteRenderer>().bounds.size.x;
+        posicionInicial = transform.position;
     }
 
     void Update()
     {
         transform.position += Vector3.left * velocidad * Time.deltaTime;
+        transform.Rotate(0f, 0f, velocidadRotacion * Time.deltaTime);
+
         if (transform.position.x <= -ancho)
         {
-            transform.position += new Vector3(ancho * 2f, 0f, 0f);
+            transform.position = posicionInicial;
+            transform.rotation = Quaternion.identity;
         }
     }
 }
