c2289f59c38c76eefd80d4fc3e836d2ef48f107c
gabrielmc05 <gabo.manzo.c@gmail.com>
2026-04-24 10:00:42 -0600

    Correction

--- Assets/Scripts/Scripts_pantalla_principal/CambiaEscena.cs
+++ Assets/Scripts/Scripts_pantalla_principal/CambiaEscena.cs
@@ -5,15 +5,12 @@ public class CambiaEscena : MonoBehaviour
 {
     public string sceneToLoad;
 
-    // 👉 Referencia opcional al barco
     public Transform barco;
 
-    // 👉 Activa esto SOLO en el botón de entrar a tienda
     public bool guardarPosicionAntesDeCambiar = false;
 
     public void LoadNuevaEscena()
     {
-        // 🔥 Guardar posición SOLO si está activado
         if (guardarPosicionAntesDeCambiar && barco != null)
         {
             GameData.posicionBarco = barco.position;
