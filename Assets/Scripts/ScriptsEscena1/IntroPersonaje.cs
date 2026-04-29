28c9edf23d906aa696c23caac671f4a6362d4e64
gabrielmc05 <gabo.manzo.c@gmail.com>
2026-04-29 01:54:53 -0600

    Fixed characters animation when waking up and now it only wakes up in Escena1

--- Assets/Scripts/ScriptsEscena1/IntroPersonaje.cs
+++ Assets/Scripts/ScriptsEscena1/IntroPersonaje.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IntroPersonaje : MonoBehaviour
 {
-    public MonoBehaviour controlMovimiento; 
-    public CambiaAnimacionPersonaje animScript; 
-
+    public MonoBehaviour controlMovimiento;
     public float duracionAnimacion = 2.5f;
+    private Animator animator;
 
     void Start()
     {
-        controlMovimiento.enabled = false;
+        animator = GetComponent<Animator>();
 
-        GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
-
-        Invoke("ActivarControl", duracionAnimacion);
+        if (SceneManager.GetActiveScene().name == "Escena1")
+        {
+            controlMovimiento.enabled = false;
+            animator.SetTrigger("despertar");
+            Invoke("ActivarControl", duracionAnimacion);
+        }
+        else
+        {
+            controlMovimiento.enabled = true;
+        }
     }
-
     void ActivarControl()
     {
         controlMovimiento.enabled = true;
